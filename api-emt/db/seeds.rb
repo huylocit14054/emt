d25950d293625b38ff5ae730430b4781f70b0b90
peterphan1996 <peterphan_1996@live.com>
2018-06-13 15:49:34 +0700

    Add seeding

--- api-emt/db/seeds.rb
+++ api-emt/db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed projects
+3.times do |p|
+    Project.create(name: Faker::RickAndMorty.unique.location, description: Faker::RickAndMorty.unique.quote)
+end
+
+# Seed 50 users
+50.times do
+    User.create(username: Faker::Internet.user_name(6..255), email: Faker::Internet.email(6..255), password: "123456")
+end
+
+# Seed project member
+(1..50).each do |member|
+    ProjectMember.create(user_id: member , project_id: (1..3).to_a.sample, role: ["project_admin","member"].sample)
+end
+
+# Seed selection dimension
+4.times do
+    dimension = Dimension.create(name: "UTM_" + Faker::Hacker.unique.abbreviation, category: "selection", project_id: rand(1..3))
+    10.times do
+        Option.create(name: Faker::LeagueOfLegends.unique.champion, dimension: dimension)
+    end
+end
+
+# Seed input dimension
+2.times do
+    Dimension.create(name: "UTM_" + Faker::Hacker.unique.abbreviation, category: "input", project_id: rand(1..3))
+end
+
+# Seed authorization
+20.times do
+    begin
+        Authorization.create(project_member_id: rand(1..50), dimension_id: rand(1..6))
+    rescue
+        retry
+    end
+end
+
+# Seed option authorization
+10.times do
+    begin
+        OptionAuthorization.create(authorization_id: rand(1..20), option_id: rand(1..40))
+    rescue
+        retry
+    end
+    
+end
