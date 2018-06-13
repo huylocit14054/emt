9ac3b0fa8e64bc5e401c289788023363d492ae2d
nenjamin2405 <nenjamin2405@gmail.com>
2018-06-13 22:21:32 +0700

    initialize client-side using antd less, fix production problem missing style jsx, integrate client side progress bar

---
+++ client-emt/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import stylesheet from './styles/antd.less'
+export default ({ children }) =>
+  <div>
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta charSet='utf-8' />
+      <style>{stylesheet}</style>
+    </Head>
+ 
+    {children}
+  </div>
---
+++ client-emt/pages/_app.js
@@ -0,0 +1,28 @@
+import App, { Container } from 'next/app';
+import React from 'react';
+import AntdLayout from '../index';
+import ProgressBar from '../components/progressBar'
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <AntdLayout>
+          <ProgressBar>
+            <Component {...pageProps} />
+          </ProgressBar>
+        </AntdLayout>
+      </Container>
+    );
+  }
+}
