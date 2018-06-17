7a33b2e11aa066b7fa6a8cf0647f19a8783e497a
Nha Hoang <hdnha11@gmail.com>
2018-06-18 00:10:46 +0700

    - Dockerize the apps for development - Add dotenv gem

--- client-emt/constants.js
+++ client-emt/constants.js
@@ -1,7 +1,7 @@
 // const API_URL = 'http://localhost:3004';
 // const CLIENT_URL = 'http://localhost:3000';
-const API_URL = 'http://localhost:3004';
-const CLIENT_URL = 'http://localhost:3000';
+const API_URL = process.env.API_URL || 'http://localhost:3004';
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 const CLOUD_NAME = 'ezgamio';
 module.exports = {
   API_URL,
