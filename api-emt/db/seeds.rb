108e1833546ab64fa536dd6ed95328b289d2be19
peterphan1996 <peterphan_1996@live.com>
2018-06-19 09:26:03 +0700

    Refactor database seed

--- api-emt/db/seeds.rb
+++ api-emt/db/seeds.rb
@@ -1,56 +1,80 @@
 
 
-# Seed 50 users
+# Seed 21 users
+puts "Create User"
 User.create(username: "quangnhat", email: "quangnhat@gmail.com", role: "root_admin", password: "quangnhat")
-50.times do
+20.times do
     User.create(username: Faker::Internet.user_name(6..255), email: Faker::Internet.email(6..255), password: "123456")
 end
 
 # Seed projects
 10.times do |p|
    project =  Project.create(name: Faker::RickAndMorty.unique.location, description: Faker::RickAndMorty.unique.quote)
+   puts "Create project #{project.name}"
    ProjectMember.create(user_id: 1 , project_id: project.id, role: "project_admin")
-   ProjectMember.create(user_id: rand(2..51) , project_id: project.id, role: "project_admin")
-   # Seed project member
-    6.times do |member|
-        begin
-        ProjectMember.create(user_id: rand(2..51) , project_id: project.id, role: "member")
+   ProjectMember.create(user_id: rand(2..21) , project_id: project.id, role: "project_admin")
+    # Seed selection dimension
+    7.times do |i|
+        begin 
+            dimension = Dimension.create(name: "UTM_" + Faker::Hacker.abbreviation , category: "selection", project: project)
+            raise unless dimension.save
+        rescue 
+            retry
+        end
+        10.times do
+            begin
+                option = Option.create(name: Faker::LeagueOfLegends.champion, dimension: dimension)
+                raise unless option.save   
+            rescue
+                retry
+            end 
+        end
+    end 
+
+
+    # Seed input dimension
+    3.times do |i|
+        begin 
+            dimension = Dimension.create(name: "UTM_" + Faker::Hacker.abbreviation, category: "input", project: project)
+            raise unless dimension.save
         rescue
             retry
-        end 
+        end
     end
-end
 
-
-
-# Seed selection dimension
-4.times do
-    dimension = Dimension.create(name: "UTM_" + Faker::Hacker.unique.abbreviation, category: "selection", project_id: rand(1..3))
+    dimensions = project.dimensions
+    # Seed project member
     10.times do
-        Option.create(name: Faker::LeagueOfLegends.unique.champion, dimension: dimension)
+        begin
+            member = ProjectMember.create(user_id: rand(2..21) , project: project, role: "member")
+            raise unless member.save
+        rescue
+            retry
+        end
+
+        # Seed authorization
+        5.times do
+            begin
+                dimension = dimensions.sample
+                authorization = Authorization.create(project_member: member , dimension: dimension)
+                raise unless authorization.save
+            rescue
+                retry
+            end
+            if (dimension.category == "selection") 
+                options = dimension.options
+                # Seed option authorization
+                5.times do
+                    begin
+                        option_auth = OptionAuthorization.create(authorization: authorization, option: options.sample)
+                        raise unless option_auth.save
+                    rescue
+                        retry
+                    end
+                end
+            end
+        end
     end
 end
 
-# Seed input dimension
-2.times do
-    Dimension.create(name: "UTM_" + Faker::Hacker.unique.abbreviation, category: "input", project_id: rand(1..3))
-end
 
-# Seed authorization
-20.times do
-    begin
-        Authorization.create(project_member_id: rand(1..50), dimension_id: rand(1..6))
-    rescue
-        retry
-    end
-end
-
-# Seed option authorization
-10.times do
-    begin
-        OptionAuthorization.create(authorization_id: rand(1..20), option_id: rand(1..40))
-    rescue
-        retry
-    end
-    
-end
