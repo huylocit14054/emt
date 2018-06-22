13c5937ee3552588180c072cdf579bf0ea777914
Nhat Truong <nenjamin2405@Nhats-MacBook-Pro.local>
2018-06-22 16:01:56 +0700

    change constants

--- client-emt/constants.js
+++ client-emt/constants.js
@@ -1,6 +1,6 @@
 // const API_URL = 'http://localhost:3004';
 // const CLIENT_URL = 'http://localhost:3000';
-const API_URL = process.env.API_URL || 'http://192.168.0.101:3004';
+const API_URL = process.env.API_URL || 'http://localhost:3004';
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 const CLOUD_NAME = 'ezgamio';
 module.exports = {
