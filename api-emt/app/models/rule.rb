c2fde72ceeae0ab9c803a947bc49a25c93742b41
huylocit14054 <sasuke1996vn@gmail.com>
2018-06-26 14:09:43 +0700

    create check validate function (#18)

--- api-emt/app/models/rule.rb
+++ api-emt/app/models/rule.rb
@@ -1,16 +1,34 @@
 class Rule < ApplicationRecord
   belongs_to :project
 
-  validate :valid
-  
-  REGEX_GET_DIMENSION_NAME = /<<(.*?)>>/
+  validate :check_rule_string_url, :check_dimension_in_rule
+
+  REGEX_GET_DIMENSION_IDS = /<<(\d*?)>>/ # get 1|2|3
+  REGEX_GET_DIMENSION_CODE = /(<<\d*?>>)/ # get <<1>>|<<2>>|<<3>>
+  REGEX_CHECK_VALIDATE_URL = /([-a-zA-Z0-9@:%_\+.~#?&=]+)/ # valid "url?utm_source=--&utm_camp="
 
   private
 
-  # check rule validate 
-  def valid
+  # check rule validation
+  def check_rule_string_url
     rule = rule_string
-    dimension_names = rule.scan(REGEX_GET_DIMENSION_NAME).flatten
-    puts dimension_names
+    rule_without_code = rule.gsub(REGEX_GET_DIMENSION_CODE, '')
+    # get all the valid url syntax and check the size with the rule_without_code original size
+    rule_without_code[REGEX_CHECK_VALIDATE_URL].size == rule_without_code.size ? true : errors.add(:rule_string, 'Invalid URL format')
+  end
+
+  # check dimension validation in rule
+  def check_dimension_in_rule
+    rule = rule_string
+    # get all dimension id in rule
+    dimension_ids = rule.scan(REGEX_GET_DIMENSION_IDS).flatten
+    # get all dimension id of the project
+    project_dimension_ids = Project.find(project_id).dimensions.ids
+    # dimension_ids - project_dimension_ids = [] => dimension_ids all include in the project => validate
+    if (dimension_ids - project_dimension_ids).empty?
+      true
+    else
+      errors.add(:rule_string, 'Dimension dose not in the project')
+    end
   end
 end
