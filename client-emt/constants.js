c7ccdf299a94a4385627c5513c0f6c6c500147b7
Nha Hoang <hdnha11@gmail.com>
2018-06-26 18:22:10 +0700

    Env config specific for browser

--- client-emt/constants.js
+++ client-emt/constants.js
@@ -1,7 +1,7 @@
 const dev = process.env.NODE_ENV === 'development';
 
-const API_URL = dev ? 'http://localhost:3004' : process.env.API_URL;
-const CLIENT_URL = dev ? 'http://localhost:3000' : process.env.CLIENT_URL;
+const API_URL = dev && process.browser ? 'http://localhost:3004' : process.env.API_URL;
+const CLIENT_URL = dev && process.browser ? 'http://localhost:3000' : process.env.CLIENT_URL;
 const CLOUD_NAME = 'ezgamio';
 
 module.exports = {
