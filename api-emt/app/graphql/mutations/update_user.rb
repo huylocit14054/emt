046c1d055ea1d5b8b18ad7cfa79bb056371b708d
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2018-06-28 13:54:47 +0700

    Implement generate_url and fix dimension assignment bug

--- api-emt/app/graphql/mutations/update_user.rb
+++ api-emt/app/graphql/mutations/update_user.rb
@@ -1,18 +1,17 @@
 class Mutations::UpdateUser < Mutations::BaseMutation
+  argument :attributes, Types::Json, required: true
 
-    argument :attributes, Types::Json, required: true
-    
-    field :updated_user, Types::User, null: true
-    
-    def resolve(attributes:)
-      user = User.find(attributes["id"])
-      is_updated = user.update_attributes(attributes)
-      if is_updated
-        {
-            updated_user: user.reload,
-        }
-      else
-        GraphQL::ExecutionError.new(user.errors.full_messages.join(", "))
-      end
+  field :updated_user, Types::User, null: true
+
+  def resolve(attributes:)
+    user = User.find(attributes['id'])
+    is_updated = user.update(attributes)
+    if is_updated
+      {
+        updated_user: user.reload
+      }
+    else
+      GraphQL::ExecutionError.new(user.errors.full_messages.join(', '))
     end
   end
+end
--- api-emt/app/models/utm.rb
+++ api-emt/app/models/utm.rb
@@ -1,9 +1,34 @@
 class Utm < ApplicationRecord
   belongs_to :project_member
 
-  # base on the values the user return generate new link
-  # example hash value: values: {"url": google.com}
-  def generate_url(values:)
-
-  end 
+  REGEX_GET_DIMENSION_IDS = /<<(\d*?)>>/ # get 1|2|3
+  REGEX_GET_DIMENSION_CODE = /(<<\d*?>>)/ # get <<1>>|<<2>>|<<3>>
+  REGEX_GET_DATE_CODE = /(<<date>>)/ # get code <<date>>
+  # REGEX_CHECK_VALID_URL = %r/^((https?):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+  # base on the values the user return generate new link base on the apllied rule
+  # example hash value:
+  # values: {
+  #   "id": 1,
+  #   "url": "https://google.com.vn",
+  #   "attributes":{
+  #    "1": "FB",
+  #    "2": "Google",
+  #    "3": "This is my link",
+  #    "4": ""
+  #   }
+  # }
+  def self.generate_url(values:)
+    utm = Utm.new
+    # get the rule
+    id = values['id']
+    url = values['url']
+    # delete the last character '/' if it exist
+    url[url.rindex('/')] = '' if url.end_with?('/')
+    attributes = value['attributes']
+    rule = Rule.find(id).rule_string
+    attributes.each do |key, value|
+      key = '<<' + key + '>>'
+      rule = rule.gsub(key, value)
+    end
+  end
 end
