264e55efec186940c1cc6d15fff5fa45f88e33a6
Nhat Truong <nenjamin2405@Nhats-MacBook-Pro.local>
2018-07-26 09:24:58 +0700

    change default logo while creating company

--- api-emt/db/migrate/20180723024258_create_companies.rb
+++ api-emt/db/migrate/20180723024258_create_companies.rb
@@ -3,7 +3,7 @@ class CreateCompanies < ActiveRecord::Migration[5.1]
     create_table :companies do |t|
       t.string :name
       t.string :address
-      t.string :logo
+      t.string :logo, default: 'logo-ex-4_g8s8im.png'
       t.string :description
       t.timestamps
     end
