8953996c68ec84d874279fd086111cab78357fab
peterphan1996 <peterphan_1996@live.com>
2018-08-06 11:30:42 +0700

    Fix error on input of invalid email, add environment variable

--- api-emt/app/models/company_member.rb
+++ api-emt/app/models/company_member.rb
@@ -23,8 +23,8 @@ class CompanyMember < ApplicationRecord
 
   def self.create_staff(company_id:, user_email:, roles:)
     user = User.find_by(email: user_email)
-    user ||= User.create(email: user_email, username: user_email, password: User.new_token)
-    company_member = CompanyMember.create(user: user, company_id: company_id, roles: roles)
+    user ||= User.create!(email: user_email, username: user_email, password: User.new_token)
+    company_member = CompanyMember.create!(user: user, company_id: company_id, roles: roles)
     CompanyMemberMailer.company_member_activation(user).deliver_now if company_member.valid?
     company_member
   end
