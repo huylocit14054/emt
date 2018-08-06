12aa297f2be757b27c3c915d433e29b33b26f42b
peterphan1996 <peterphan_1996@live.com>
2018-08-06 12:48:40 +0700

    Fix rubocop

--- api-emt/config/environments/development.rb
+++ api-emt/config/environments/development.rb
@@ -60,11 +60,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
+    address: 'smtp.sendgrid.net',
     port: 587,
-    authentication: "plain",
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"],
+    authentication: 'plain',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'localhost:1025',
     enable_starttls_auto: true
   }
